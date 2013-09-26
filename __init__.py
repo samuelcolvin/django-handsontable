64f433b2ea6bea5eb8179b22ec5ff356ff561685
scolvin <s@muelcolvin.com>
2013-09-26 20:41:12 +0100

    small changes while working on other project

--- __init__.py
+++ __init__.py
@@ -6,9 +6,9 @@ __version__ = '0.1'
 class _MetaBaseDisplayModel(type):
     def __init__(cls, *args, **kw):
         type.__init__(cls, *args, **kw)
-        if not (hasattr(cls,'HotTable') or hasattr(cls,'DjangoTable')):
+        if cls.__name__ in ('BaseDisplayModel', 'ModelDisplay'):
             return
-        assert hasattr(cls, 'model'), '%s is missing a model, all display models must have a model attribute at %s' % (cls.__name__, cls.__file__)
+        assert hasattr(cls, 'model'), '%s is missing a model, all display models must have a model attribute' % cls.__name__
         cls.model_name = cls.model.__name__
         if hasattr(cls, 'HotTable'):
             if hasattr(cls.HotTable, 'Meta'):
@@ -73,10 +73,10 @@ def get_rest_apps():
             del disp_app
     return display_apps
 
-def find_model(apps, to_fild):
+def find_model(apps, to_find):
     for app in apps.values():
         for disp_model in app.values():
-            if disp_model.model == to_fild:
+            if disp_model.model == to_find:
                 return disp_model
     return None
 
